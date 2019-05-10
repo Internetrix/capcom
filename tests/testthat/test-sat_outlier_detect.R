b0914276684579d60dca175b94b2a29cf7ea19e2
Dean Marchiori <dean.marchiori@internetrix.com.au>
2019-05-10 11:01:24 +1000

    added tests for outlier detect

--- tests/testthat/test-sat_outlier_detect.R
+++ tests/testthat/test-sat_outlier_detect.R
@@ -1,5 +1,40 @@
 context("test-sat_outlier_detect")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+data("google_analytics_data")
+
+input <- google_analytics_data
+
+fit <- sat_outlier_detect(x =  input$conversion_rate,
+                          wt = input$sessions,
+                          index = input$ds,
+                          tr = TRUE)
+
+
+test_that("returns a data frame", {
+  expect_true(is.data.frame(fit))
 })
+
+test_that("output has 6 cols", {
+  expect_equal(ncol(fit), 6)
+})
+
+test_that("output has same number of rows as input", {
+  expect_equal(nrow(input), nrow(fit))
+})
+
+test_that("output has correct colnames", {
+  expect_equal(colnames(fit), c("index", "data", "residuals", "resid_lower", "resid_upper", "outlier"))
+})
+
+test_that("outlier col is factor", {
+  expect_true(is.factor(fit$outlier))
+})
+
+
+
+
+
+
+
+
+
