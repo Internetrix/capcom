16f9331bbe1f653b65b32fa8101a94593ef759c9
Dean Marchiori <dean.marchiori@internetrix.com.au>
2019-05-10 11:16:17 +1000

    added tests for sat_pca

--- tests/testthat/test-sat_pca.R
+++ tests/testthat/test-sat_pca.R
@@ -1,5 +1,36 @@
 context("test-sat_pca")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+data("ga_sourcemedium_wide")
+input <- ga_sourcemedium_wide
+x <- sat_pca(x = input, min_nonzero = 1, min_val = 0.01)
+
+test_that("output is a list", {
+  expect_true(is.list(x))
 })
+
+test_that("output is a list", {
+  expect_equal(length(x), 4)
+})
+
+test_that("1st element is a data frame", {
+  expect_true(is.data.frame(x[[1]]))
+})
+
+test_that("1st element has same num rows as input", {
+  expect_equal(nrow(x[[1]]), nrow(input))
+})
+
+test_that("1st element has one fewed col than input", {
+  expect_equal(ncol(x[[1]]), ncol(input)-1)
+})
+
+test_that("2nd element is numeric", {
+  expect_true(is.numeric(x[[2]]))
+})
+
+test_that("2nd element dimensionality correct", {
+  expect_equal(dim(x[[2]]), dim(input) -c(0,1))
+})
+
+
+
